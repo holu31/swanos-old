92daaa85ae83043958b1868de26581e346f52a0c
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-04-17 20:03:34 +0300

    Add files via upload

--- build.bat
+++ build.bat
@@ -3,7 +3,6 @@ i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/ports.c -o
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/console.c -o bin/io/console.o
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/kernel/kernel.c -o bin/kernel/kernel.o
 i686-elf-as src/boot/kernel_entry.s -o bin/boot/kernel_entry.o
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -nostdlib -lgcc -T link.ld -o build/boot/kernel.elf bin/kernel/kernel.o bin/boot/kernel_entry.o bin/io/console.o
-wsl grub-mkrescue -o kernel.iso build/
-qemu-system-i386 -m 32 -cdrom kernel.iso -monitor stdio -no-reboot 
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -nostdlib -lgcc -T link.ld -o bin/kernel.elf bin/kernel/kernel.o bin/boot/kernel_entry.o bin/io/console.o
+qemu-system-i386 -kernel bin/kernel.elf
 pause 
